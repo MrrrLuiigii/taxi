348b158dfb7b721450489d54098508d09830076b
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-10-08 09:29:03 +0200

    render map

--- src/components/map-box-component.js
+++ src/components/map-box-component.js
@@ -1,5 +1,6 @@
 import { html } from "lit-element";
 import { BaseView } from "../views/base-view.js";
+import mapboxgl from "mapbox-gl";
 
 class MapBoxComponent extends BaseView {
   static get properties() {
@@ -8,8 +9,37 @@ class MapBoxComponent extends BaseView {
     };
   }
 
+  firstUpdated() {
+    this.loadMapBox();
+  }
+
+  loadMapBox() {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoibXJycmx1aWlnaWkiLCJhIjoiY2tnMGhvb2ZvMDN0bjJybnhseHprNGE4eSJ9.S9RMbJT6yNX6DAwdcHG9vA";
+    // mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+
+    this.map = new mapboxgl.Map({
+      container: "mapContainer",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [5.704076, 51.477491],
+      zoom: 10,
+    });
+  }
+
   render() {
-    return html` <h2>MAPBOX</h2> `;
+    return html`
+      <div id="mapContainer"></div>
+
+      <style>
+        #mapContainer {
+          width: 640px;
+          height: 480px;
+
+          border: 2px solid var(--dark-color);
+          border-radius: 10px;
+        }
+      </style>
+    `;
   }
 }
 
